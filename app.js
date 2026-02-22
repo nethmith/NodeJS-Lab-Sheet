a865dfb0568b22f8d75e5ed6071f61fd14274388
nethmith <nethmiherath100@gmail.com>
2026-02-22 10:45:48 +0530

    Added async/await feature

--- app.js
+++ app.js
@@ -1,2 +1,20 @@
-const myModule = require('./my-module.js');
-console.log(myModule.myFunction()); 
+const condition = true;
+
+const myPromise = new Promise((resolve, reject) => {
+    if (condition) {
+        resolve('Success!');
+    } else {
+        reject('Failure!');
+    }
+});
+
+async function myFunction() {
+    try {
+        const result = await myPromise;
+        console.log(result);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+myFunction();
